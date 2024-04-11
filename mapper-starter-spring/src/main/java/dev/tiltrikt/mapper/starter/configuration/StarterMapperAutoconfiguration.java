15c58352a2154b1cd7ebb8a71ec7aad3833a95ee
Tiltrikt <vladoss339@gmail.com>
2024-04-11 23:38:28 +0200

    starter

---
+++ mapper-starter-spring/src/main/java/dev/tiltrikt/mapper/starter/configuration/StarterMapperAutoconfiguration.java
@@ -0,0 +1,23 @@
+package dev.tiltrikt.mapper.starter.configuration;
+
+import dev.tiltrikt.mapper.core.Mapper;
+import dev.tiltrikt.mapper.core.MapperImpl;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class StarterMapperAutoconfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean(Mapper.class)
+  public Mapper mapper() {
+    return new MapperImpl();
+  }
+
+}
