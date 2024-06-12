27aced1b7ef4976c574460f20a3a5abfc1bc4ab2
Tiltrikt <vladoss339@gmail.com>
2024-06-12 21:52:01 +0200

    publishing to reposilite publishing only needed directories

--- mapper-core/src/test/java/dev/tiltrikt/mapper/core/model/SimpleFieldModel.java
+++ mapper-core/src/test/java/dev/tiltrikt/mapper/core/model/SimpleFieldModel.java
@@ -1,11 +1,13 @@
 package dev.tiltrikt.mapper.core.model;
 
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Getter
+@EqualsAndHashCode
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class SimpleFieldModel {
